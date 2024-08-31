88403fe4eceac77cad3fc4419388a64d1b4e8abb
austin-b <t.austinbennett@gmail.com>
2024-08-30 23:08:33 -0400

    part 2 complete

--- 1 - Trebuchet/src/main.rs
+++ 1 - Trebuchet/src/main.rs
@@ -1,11 +1,14 @@
-use std::fs; // Filesystem
-use std::str; // String
-use std::collections::VecDeque; // Vector
+// https://adventofcode.com/2023/day/1
+
+use std::collections::VecDeque;
+use std::fs;
+use std::str;
 
 fn main() {
-
-    let contents: String = fs::read_to_string("input.txt")
+    let contents: String = fs::read_to_string("src/input.txt")
         .expect("Something went wrong reading the file");
+
+    //let contents = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
 
     let lines: str::Lines<'_> = contents.lines();
 
@@ -16,18 +19,61 @@ fn main() {
     }
 
     println!("Sum: {}", sum);
-
 }
 
 fn process_line_for_number(line: &str) -> u32 {
     let mut numbers: VecDeque<char> = VecDeque::new();
 
     println!("new line: {}", line);
-    for c in line.chars() {
+
+    for (i, c) in line.chars().enumerate() {
         if c.is_numeric() {
             numbers.push_back(c);
+        } else {
+            if c == 'o' {
+                if let Some("one") = line.get(i..i + 3) {
+                    numbers.push_back('1');
+                    println!("one found");
+                }
+            } else if c == 't' {
+                if let Some("two") = line.get(i..i + 3) {
+                    numbers.push_back('2');
+                    println!("two found");
+                }
+                if let Some("three") = line.get(i..i + 5) {
+                    numbers.push_back('3');
+                    println!("three found");
+                }
+            } else if c == 'f' {
+                if let Some("four") = line.get(i..i + 4) {
+                    numbers.push_back('4');
+                    println!("four found");
+                }
+                if let Some("five") = line.get(i..i + 4) {
+                    numbers.push_back('5');
+                    println!("five found");
+                }
+            } else if c == 's' {
+                if let Some("six") = line.get(i..i + 3) {
+                    numbers.push_back('6');
+                    println!("six found");
+                }
+                if let Some("seven") = line.get(i..i + 5) {
+                    numbers.push_back('7');
+                    println!("seven found");
+                }
+            } else if c == 'e' {
+                if let Some("eight") = line.get(i..i + 5) {
+                    numbers.push_back('8');
+                    println!("eight found");
+                }
+            } else if c == 'n' {
+                if let Some("nine") = line.get(i..i + 4) {
+                    numbers.push_back('9');
+                    println!("nine found");
+                }
+            }
         }
-        // part two processing
     }
 
     let number: u32;
@@ -37,7 +83,7 @@ fn process_line_for_number(line: &str) -> u32 {
     if numbers.len() > 1 {
         let mut twodigit = String::new();
         twodigit.push(numbers.pop_front().unwrap()); // First number
-        twodigit.push(numbers.pop_back().unwrap());  // Last number
+        twodigit.push(numbers.pop_back().unwrap()); // Last number
         number = twodigit.parse().unwrap();
     } else {
         let onedigit = numbers.pop_front().unwrap(); // Only number
