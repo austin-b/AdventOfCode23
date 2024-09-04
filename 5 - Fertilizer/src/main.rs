249945544ad9f47e87f4d9a5d5e64d7c363d7863
tommyboi <t.austinbennett@gmail.com>
2024-09-04 11:14:32 -0400

    day 5 init

---
+++ 5 - Fertilizer/src/main.rs
@@ -0,0 +1,13 @@
+// https://adventofcode.com/2023/day/5
+
+
+
+use std::fs;
+
+fn main() {
+
+    let contents = fs.read_to_string("src/example.txt")
+        .expect("Something went wrong reading the file");
+
+    println!("Hello, world!");
+}
