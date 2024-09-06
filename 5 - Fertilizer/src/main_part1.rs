5064f73508a2916d61829ddf504e503cbd33f1ea
tommyboi <t.austinbennett@gmail.com>
2024-09-06 11:17:04 -0400

    seems to work, need optimize - takes >30 minutes

--- 5 - Fertilizer/src/main_part1.rs
+++ 5 - Fertilizer/src/main_part1.rs
@@ -1,20 +1,20 @@
 // https://adventofcode.com/2023/day/5
-// PART 2
+// PART 1
 
 #[allow(dead_code)]
 
 use std::fs;
-use std::{collections::VecDeque, ops::RangeInclusive};
+use std::ops::RangeInclusive;
 
 fn main() {
 
-    //let contents = fs::read_to_string("src/example.txt")
-    //    .expect("Something went wrong reading the file");
+    // let contents = fs::read_to_string("src/example.txt")
+    //     .expect("Something went wrong reading the file");
 
     let contents = fs::read_to_string("src/input.txt")
-       .expect("Something went wrong reading the file");
+        .expect("Something went wrong reading the file");
 
-    let mut seed_ranges: Vec<RangeInclusive<u64>> = Vec::new();
+    let mut seeds: Vec<u64> = Vec::new();
     let mut seed_to_soil: Vec<(RangeInclusive<u64>, u64)> = Vec::new();
     let mut soil_to_fertilizer: Vec<(RangeInclusive<u64>, u64)> = Vec::new();
     let mut fertilizer_to_water: Vec<(RangeInclusive<u64>, u64)> = Vec::new();
@@ -34,18 +34,8 @@ fn main() {
 
     for b in blocks {
         if b.contains("seeds") {
-            println!("b: {:?}", b);
-            let mut seed_nums: VecDeque<u64> = b.split(": ").collect::<Vec<&str>>()[1].split_whitespace().into_iter().map(|x| x.trim().parse().unwrap()).collect();
-
-            loop {
-                let start = seed_nums.pop_front();
-                let length = seed_nums.pop_front();
-                if start.is_none() || length.is_none() { break; }
-
-                seed_ranges.push(RangeInclusive::new(start.unwrap(), start.unwrap() + length.unwrap()));
-            }
-
-            println!("seed_ranges: {:?}", seed_ranges);
+            seeds = b.split(": ").collect::<Vec<&str>>()[1].split_whitespace().into_iter().map(|x| x.trim().parse().unwrap()).collect::<Vec<u64>>();
+            println!("seeds: {:?}", seeds);
         } else if b.contains("seed-to-soil") {
             // this mapping is different because it is not guaranteed to start at 0
             let ranges = b.split(":\n").collect::<Vec<&str>>()[1];
@@ -61,7 +51,7 @@ fn main() {
                 soil_to_fertilizer.push((RangeInclusive::new(parts[1], parts[1] + parts[2]), parts[0]));
             }
         } else if b.contains("fertilizer-to-water") {
-            let ranges = b.split("\n").collect::<Vec<&str>>()[1];
+            let ranges = b.split(":\n").collect::<Vec<&str>>()[1];
             for r in ranges.lines() {
                 let parts = r.split_whitespace().into_iter().map(|x| x.trim().parse().unwrap()).collect::<Vec<u64>>();
                 fertilizer_to_water.push((RangeInclusive::new(parts[1], parts[1] + parts[2]), parts[0]));
@@ -101,81 +91,77 @@ fn main() {
     // println!("temperature_to_humidity: {:?}", &temperature_to_humidity);
     // println!("humidity_to_location: {:?}", &humidity_to_location);
 
-    let mut lowest: u64 = 0;
+    let mut locations: Vec<u64> = Vec::new();
 
-    for range in seed_ranges {
-        for seed in range {
-            let mut soil = 0;
-            for (range, dest) in &seed_to_soil {
-                if range.contains(&seed) {
-                    soil = dest.clone() + (seed - range.start());
-                    break;
-                }
+    for seed in seeds {
+        let mut soil = 0;
+        for (range, dest) in &seed_to_soil {
+            if range.contains(&seed) {
+                soil = dest.clone() + (seed - range.start());
+                break;
             }
-            // Any source numbers that aren't mapped correspond to the same destination number.
-            if soil == 0 { soil = seed.clone(); }
-
-            let mut fertilizer = 0;
-            for (range, dest) in &soil_to_fertilizer {
-                if range.contains(&soil) {
-                    fertilizer = dest.clone() + (soil - range.start());
-                    break;
-                }
-            }
-            if fertilizer == 0 { fertilizer = soil.clone(); }
-
-            let mut water = 0;
-            for (range, dest) in &fertilizer_to_water {
-                if range.contains(&fertilizer) {
-                    water = dest.clone() + (fertilizer - range.start());
-                    break;
-                }
-            }
-            if water == 0 { water = fertilizer.clone(); }
-
-            let mut light = 0;
-            for (range, dest) in &water_to_light {
-                if range.contains(&water) {
-                    light = dest.clone() + (water - range.start());
-                    break;
-                }
-            }
-            if light == 0 { light = water.clone(); }
-
-            let mut temperature = 0;
-            for (range, dest) in &light_to_temperature {
-                if range.contains(&light) {
-                    temperature = dest.clone() + (light - range.start());
-                    break;
-                }
-            }
-            if temperature == 0 { temperature = light.clone(); }
-
-            let mut humidity = 0;
-            for (range, dest) in &temperature_to_humidity {
-                if range.contains(&temperature) {
-                    humidity = dest.clone() + (temperature - range.start());
-                    break;
-                }
-            }
-            if humidity == 0 { humidity = temperature.clone(); }
-
-            let mut location = 0;
-            for (range, dest) in &humidity_to_location {
-                if range.contains(&humidity) {
-                    location = dest.clone() + (humidity - range.start());
-                    break;
-                }
-            }
-            if location == 0 { location = humidity.clone(); }
-
-            if lowest == 0 || location < lowest {
-                lowest = location;
-            }
-
-            // println!("seed: {}, soil: {}, fertilizer: {}, water: {}, light: {}, temperature: {}, humidity: {}, location: {}", seed, soil, fertilizer, water, light, temperature, humidity, location);
         }
+        // Any source numbers that aren't mapped correspond to the same destination number.
+        if soil == 0 { soil = seed.clone(); }
+
+        let mut fertilizer = 0;
+        for (range, dest) in &soil_to_fertilizer {
+            if range.contains(&soil) {
+                fertilizer = dest.clone() + (soil - range.start());
+                break;
+            }
+        }
+        if fertilizer == 0 { fertilizer = soil.clone(); }
+
+        let mut water = 0;
+        for (range, dest) in &fertilizer_to_water {
+            if range.contains(&fertilizer) {
+                water = dest.clone() + (fertilizer - range.start());
+                break;
+            }
+        }
+        if water == 0 { water = fertilizer.clone(); }
+
+        let mut light = 0;
+        for (range, dest) in &water_to_light {
+            if range.contains(&water) {
+                light = dest.clone() + (water - range.start());
+                break;
+            }
+        }
+        if light == 0 { light = water.clone(); }
+
+        let mut temperature = 0;
+        for (range, dest) in &light_to_temperature {
+            if range.contains(&light) {
+                temperature = dest.clone() + (light - range.start());
+                break;
+            }
+        }
+        if temperature == 0 { temperature = light.clone(); }
+
+        let mut humidity = 0;
+        for (range, dest) in &temperature_to_humidity {
+            if range.contains(&temperature) {
+                humidity = dest.clone() + (temperature - range.start());
+                break;
+            }
+        }
+        if humidity == 0 { humidity = temperature.clone(); }
+
+        let mut location = 0;
+        for (range, dest) in &humidity_to_location {
+            if range.contains(&humidity) {
+                location = dest.clone() + (humidity - range.start());
+                break;
+            }
+        }
+        if location == 0 { location = humidity.clone(); }
+
+        locations.push(location);
+
+        // println!("seed: {}, soil: {}, fertilizer: {}, water: {}, light: {}, temperature: {}, humidity: {}, location: {}", seed, soil, fertilizer, water, light, temperature, humidity, location);
     }
 
-    println!("lowest location: {:?}", lowest);
+    println!("lowest location: {:?}", locations.iter().min().unwrap());
 }
