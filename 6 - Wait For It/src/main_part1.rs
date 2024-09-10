4ac5e68c431a3112a0857bbf92bdd6fc55f6bea1
tommyboi <t.austinbennett@gmail.com>
2024-09-10 10:49:15 -0400

    day 6 part 2

--- 6 - Wait For It/src/main_part1.rs
+++ 6 - Wait For It/src/main_part1.rs
@@ -3,25 +3,30 @@
 fn main() {
 
     // EXAMPLE
-    //let content = "Time:      7  15   30\nDistance:  9  40  200";
+    // let content = "Time:      7  15   30\nDistance:  9  40  200";
 
     // INPUT
     let content = "Time:        56     97     78     75\nDistance:   546   1927   1131   1139";
 
-    let time = content.lines().nth(0).unwrap().split_whitespace().skip(1).into_iter().collect::<Vec<&str>>().join("").parse::<i64>().unwrap();
-    let distance = content.lines().nth(1).unwrap().split_whitespace().skip(1).into_iter().collect::<Vec<&str>>().join("").parse::<i64>().unwrap();
+    let time = content.lines().nth(0).unwrap().split_whitespace().skip(1).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let distance = content.lines().nth(1).unwrap().split_whitespace().skip(1).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let races = time.into_iter().zip(distance).collect::<Vec<(i32, i32)>>();
 
-    println!("time: {:?}", time);
-    println!("distance: {:?}", distance);
-    
-    let ways = ways_to_win(time, distance);
-    println!("ways to win: {:?}", ways);
+    let mut product = 1;
+    for race in races {
+        let ways = ways_to_win(race);
+        println!("ways to win for race {:?}: {:?}", race, ways);
+        product *= ways;
+    }
+
+    println!("product: {:?}", product);
 }
 
-fn ways_to_win(time: i64, distance: i64) -> i64 {
+fn ways_to_win(race: (i32, i32)) -> i32 {
+    let (time, distance) = race;
     let mut ways = 0;
 
-    let mut charge_time: i64 = 0;
+    let mut charge_time: i32 = 0;
 
     // brute force -- can probably figure this out mathematically
     loop {
@@ -46,6 +51,6 @@ figured this out mathematically:
     and velocity = charge_time
     then distance_traveled = charge_time * (total_time - charge_time)
 */
-fn distance_traveled(total_time: i64, charge_time: i64) -> i64 {
+fn distance_traveled(total_time: i32, charge_time: i32) -> i32 {
     charge_time * (total_time - charge_time)
 }
