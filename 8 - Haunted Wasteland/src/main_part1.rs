746df61e95c8123dfb06c048f5e7b2b9ac69e8c4
austin-b <t.austinbennett@gmail.com>
2024-09-11 23:21:11 -0400

    day 8 part 2 - runs forever, no result yet

--- 8 - Haunted Wasteland/src/main_part1.rs
+++ 8 - Haunted Wasteland/src/main_part1.rs
@@ -10,19 +10,14 @@ fn main() {
     // EXAMPLE 2
     // let contents = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)";
 
-    // EXAMPLE 3
-    // let contents = "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)";
-
     let contents: String = fs::read_to_string("src/input.txt")
         .expect("Something went wrong reading the file");
 
-    let pieces: Vec<&str> = contents.split("\r\n\r\n").collect();
-    // println!("{:?}", pieces);
+    let pieces: Vec<&str> = contents.split("\n\n").collect();
     let directions: &str = pieces[0];
-    let nodes_str: Vec<&str> = pieces[1].split("\r\n").collect();
+    let nodes_str: Vec<&str> = pieces[1].split("\n").collect();
 
     let mut nodes: HashMap<&str, (&str, &str)> = HashMap::new();
-    let mut current_nodes: Vec<&str> = Vec::new();
 
     for node in nodes_str {
         let node_info: Vec<&str> = node.split(" = ").collect();
@@ -31,49 +26,38 @@ fn main() {
         let node_left: &str = node_children[0].trim_matches(|c| c == '(' || c == ')');
         let node_right: &str = node_children[1].trim_matches(|c| c == '(' || c == ')');
 
-        if node_name.ends_with('A') {
-            current_nodes.push(node_name);
-        }
-
         nodes.insert(node_name, (node_left, node_right));
     }
 
     println!("Directions: {:?}", directions);
     println!("Nodes: {:?}", nodes);
-    println!("Starting nodes: {:?}", current_nodes);
 
+    let mut current_node: &str = "AAA";
     let mut step_count: i32 = 0;
+    let mut found: bool = false;
 
     // Directions can be repeated, so we loop until we find the end node
-    'top: loop {
+    loop {
         for direction in directions.chars() {
-            let mut next_step_nodes: Vec<&str> = Vec::new();
-            let mut found: bool = true; // starts with true and will be set to false if any node doesn't end with Z by the &&
+            let node = nodes.get(current_node).unwrap();
 
-            for node in current_nodes.iter() {
-                let node = nodes.get(*node).unwrap();
-                let new_node: &str;
+            println!("{:?}: {:?} {:?}", current_node, node.0, node.1);
 
-                // println!("{:?}: {:?} {:?}", node, node.0, node.1);
-
-                if direction == 'R' {
-                    new_node = node.1;
-                } else {
-                    new_node = node.0;
-                }
-
-                found = found && new_node.ends_with("Z");
-
-                next_step_nodes.push(new_node);
+            if direction == 'R' {
+                current_node = node.1;
+            } else {
+                current_node = node.0;
             }
 
-            current_nodes = next_step_nodes;
+            if current_node == "ZZZ" {
+                found = true;
+            }
 
             step_count += 1;
+        }
 
-            if found {
-                break 'top;
-            }
+        if found {
+            break;
         }
     }
 
