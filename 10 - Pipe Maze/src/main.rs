cc4dbcaf97751115aac12d961d2cddeb75d93c98
austin-b <t.austinbennett@gmail.com>
2024-09-12 23:15:54 -0400

    testing find path, outputs of next_step are not what I expect

--- 10 - Pipe Maze/src/main.rs
+++ 10 - Pipe Maze/src/main.rs
@@ -1,8 +1,8 @@
 // https://adventofcode.com/2023/day/10
 
-use std::{error::Error, f32::consts::E, thread::current};
+use std::{error::Error, f32::consts::E, path, thread::current};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 enum Tiles {
     NS,
     EW,
@@ -28,7 +28,7 @@ fn main() {
     let contents = ".....\n.S-7.\n.|.|.\n.L-J.\n.....";
 
     let mut map: Vec<Vec<Tiles>> = Vec::new();
-    let mut starting: (usize, usize) = (0, 0);
+    let mut starting: (i32, i32) = (0, 0);
 
     for (i, row) in contents.lines().enumerate() {
         let mut row_vec: Vec<Tiles> = Vec::new();
@@ -53,7 +53,7 @@ fn main() {
             }
 
             if c == 'S' {
-                starting = (i, j);
+                starting = (i.try_into().unwrap(), j.try_into().unwrap());
             }
         }
         map.push(row_vec);
@@ -63,16 +63,38 @@ fn main() {
     //     println!("{:?}", row);
     // }
     println!("{starting:?}");
+
+    let path = find_path(&map, starting);
+
+    for (pos, steps) in path {
+        println!("{pos:?} {steps:?}");
+    }
 }
 
-fn find_path(map: &Vec<Vec<Tiles>>, starting: (usize, usize)) -> Vec<((usize,usize), usize))> {
-    let mut current_tile = map[starting.0][starting.1];
-    let mut from_direction: Direction;
+fn find_path(map: &Vec<Vec<Tiles>>, starting: (i32, i32)) -> Vec<((i32, i32), usize)> {
+    let mut path: Vec<((i32, i32), usize)> = Vec::new();
+    let mut current_position: (i32, i32) = starting;
+    let mut from_direction: Option<Direction> = None;
+    let mut steps: usize = 0;
 
-    
+    // Go through the available directions and find a valid one
+    // There should only be 2 from the starting position, but we will check all 4 as we don't know initially
+    // The path goes in a loop, so it does not matter which direction we start with
+    let tests = [((1,0), Direction::N), ((0,-1), Direction::E), ((-1, 0), Direction::S), ((0,1), Direction::W)];
+    for (step, direction) in tests {
+        let next = next_step(map[(current_position.0+step.0) as usize][(current_position.1+step.1) as usize], direction);
+        println!("{next:?}");
+    }
+
+    if from_direction.is_none() {
+        panic!("No valid direction found from starting position");
+    } else { println!("{current_position:?} {from_direction:?} {steps:?}"); }
+
+    path
 }
 
-fn next_step(current_tile: Tiles, from_direction: Direction) -> Option<((i8, i8), Direction)> {
+// output: what to add to the current position, and the direction you're coming from into new position
+fn next_step(current_tile: Tiles, from_direction: Direction) -> Option<((i32, i32), Direction)> {
     match current_tile {
         Tiles::NS => {
             match from_direction {
@@ -83,8 +105,8 @@ fn next_step(current_tile: Tiles, from_direction: Direction) -> Option<((i8, i8)
         }
         Tiles::EW => {
             match from_direction {
-                Direction::E => Some(((0,1), Direction::E)),
-                Direction::W => Some(((0,-1), Direction::W)),
+                Direction::E => Some(((0,-1), Direction::E)),
+                Direction::W => Some(((0,1), Direction::W)),
                 _ => None,
             }
         }
