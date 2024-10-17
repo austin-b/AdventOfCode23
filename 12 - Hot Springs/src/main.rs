0b772b2ed601b549896b244b509d6fc86e6b7bfd
tommyboi <t.austinbennett@gmail.com>
2024-10-17 10:32:15 -0400

    implemented check_and_remove_completed_groups

--- 12 - Hot Springs/src/main.rs
+++ 12 - Hot Springs/src/main.rs
@@ -1,28 +1,78 @@
 // https://adventofcode.com/2023/day/12
 
+use rayon::prelude::*;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum SpringState {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
 fn main() {
     
     // EXAMPLE
     let contents = "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1";
 
-    let conditions: Vec<(&str, Vec<usize>)> = contents.lines().map(|line: &str| {
+    let mut conditions: Vec<(Vec<SpringState>, Vec<usize>)> = contents.lines().map(|line: &str| {
         let records: Vec<&str> = line.split(" ").collect();
+        let states: Vec<SpringState> = records[0].chars().map(|spring| {
+            match spring {
+                '?' => SpringState::Unknown,
+                '.' => SpringState::Operational,
+                '#' => SpringState::Damaged,
+                _ => panic!("Invalid character"),
+            }
+        }).collect();
         let mut groups: Vec<usize> = records[1].split(",").map(|group| group.parse::<usize>().unwrap()).collect();
         groups.sort();
-        (records[0], groups)
+        (states, groups)
     }).collect();
 
-    for record in &conditions {
+    for record in &mut conditions {
+        check_and_remove_completed_groups(&record.0, &mut record.1);
         println!("{:?}", record);
     }
 
-    let arrangements: Vec<usize> = find_number_of_arrangements(&conditions);
+    // let sum: usize = conditions.par_iter().map(|r| find_number_of_arrangements(r)).sum();
 
-    println!("\nsum of arrangements: {:?}", arrangements.iter().sum::<usize>());
+    // println!("\nsum of arrangements: {:?}", sum);
 }
 
-fn find_number_of_arrangements(conditions: &Vec<(&str, Vec<usize>)>) -> Vec<usize> {
-    
+// TODO: implement some version of Heap's algorithm?
+fn find_number_of_arrangements(record: &(Vec<SpringState>, Vec<usize>)) -> usize {
+    let mut states = &record.0;
+    let mut groups = &record.1;
 
-    vec![1,2,3]
+
+
+    1
+}
+
+fn check_and_remove_completed_groups(states: &Vec<SpringState>, groups: &mut Vec<usize>) {
+
+    let mut countable: bool = true;
+    let mut count: usize = 0;
+
+    for state in states {
+        match state {
+            &SpringState::Damaged => if countable {count += 1},
+            &SpringState::Unknown => countable = false,
+            &SpringState::Operational => {
+                countable = true;
+    
+                if count > 0 {
+                    println!("found a grouping of {}", count);
+                    if let Ok(index) = groups.binary_search(&count) { groups.remove(index); }
+                }
+    
+                count = 0;
+            }
+        }
+    }
+
+    if countable && count > 0 {
+        println!("found a grouping of {}", count);
+        if let Ok(index) = groups.binary_search(&count) { groups.remove(index); }
+    }
 }
