ba9f425e51a2f5ebe72cf3274483a4fe34a29424
tommyboi <t.austinbennett@gmail.com>
2024-10-29 09:27:00 -0400

    day 12 part 1 complete

--- 12 - Hot Springs/src/main.rs
+++ 12 - Hot Springs/src/main.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2023/day/12
 
-use std::{collections::HashMap, fs};
+use std::{collections::VecDeque, fs};
 use rayon::prelude::*;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -13,11 +13,11 @@ enum SpringState {
 fn main() {
     
     // EXAMPLE
-    let contents = "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1";
+    // let contents = "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1";
 
     // PUZZLE
-    // let contents: String = fs::read_to_string("src/input.txt")
-    //     .expect("Something went wrong reading the file");
+    let contents: String = fs::read_to_string("src/input.txt")
+        .expect("Something went wrong reading the file");
 
     let conditions: Vec<(Vec<SpringState>, Vec<usize>)> = contents.lines().map(|line: &str| {
         let records: Vec<&str> = line.split(" ").collect();
@@ -32,18 +32,6 @@ fn main() {
         let groups: Vec<usize> = records[1].split(",").map(|group| group.parse::<usize>().unwrap()).collect();
         (states, groups)
     }).collect();
-
-    // for record in &mut conditions {
-    //     println!("complete: {}", check_if_complete(&record.0, &mut record.1));
-    //     println!("{:?}", record);
-    // }
-
-    // let mut sum: usize = 0;
-    // for record in &conditions {
-    //     let r = find_number_of_arrangements(record);
-    //     println!("sum of arrangements for line {:?}: {:?}\n", record.0, r);
-    //     sum += r;
-    // }
     
     let sum: usize = conditions.par_iter().map(|r| find_number_of_arrangements(r)).sum();
 
@@ -51,19 +39,37 @@ fn main() {
 }
 
 fn find_number_of_arrangements(record: &(Vec<SpringState>, Vec<usize>)) -> usize {
+    println!("record: {:?}", record.0);
+
     let states: &Vec<SpringState> = &record.0;
     let groups: &Vec<usize> = &record.1;
+
+    let unknowns: VecDeque<usize> = states.iter().enumerate().filter(|(_, &s)| s == SpringState::Unknown).map(|(i, _)| i).collect();
+
+    check_permutations(states.clone(), groups.clone(), unknowns)
+}
+
+fn check_permutations(states: Vec<SpringState>, groups: Vec<usize>, mut unknowns: VecDeque<usize>) -> usize {
     let mut arrangements: usize = 0;
-    for (loc, state) in states.clone().into_iter().enumerate() {
-        if state == SpringState::Unknown {
-            let mut states_copy_damaged = states.clone();
-            states_copy_damaged[loc] = SpringState::Damaged;
-            let mut states_copy_operational = states.clone();
-            states_copy_operational[loc] = SpringState::Operational;
-            arrangements = find_number_of_arrangements(&(states_copy_damaged, groups.clone())) + find_number_of_arrangements(&(states_copy_operational, groups.clone()));
-        }
+
+    let next_unknown = unknowns.pop_front();
+
+    if next_unknown.is_some() {
+        // change next unknown to damaged
+        let mut states_copy_damaged = states.clone();
+        states_copy_damaged[next_unknown.unwrap()] = SpringState::Damaged;
+
+        // change next unknown to operational
+        let mut states_copy_operational = states.clone();
+        states_copy_operational[next_unknown.unwrap()] = SpringState::Operational;
+
+        // recursively check both permutations
+        arrangements = check_permutations(states_copy_damaged, groups.clone(), unknowns.clone()) + check_permutations(states_copy_operational, groups.clone(), unknowns.clone());
+    } else {
+        // if no more unknowns, check if the arrangement is complete
+        arrangements += if check_if_complete(&states, &groups) {1} else {0};
     }
-    arrangements += if check_if_complete(&states, &groups) {1} else {0};
+    
     arrangements
 }
 
